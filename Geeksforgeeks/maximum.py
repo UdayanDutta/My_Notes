2045eace81738e898bdd4a84b7fefaf91dba5294
UdayanDutta <137642006+UdayanDutta@users.noreply.github.com>
2023-07-08 13:11:53 +0530

    maximum.py

--- Geeksforgeeks/maximum.py
+++ Geeksforgeeks/maximum.py
@@ -13,3 +13,13 @@ b=-4
 maximum=max(a,b)
 
 print("The maximum number is",maximum)
+
+
+
+//user given number
+
+number1 = int(input("First number: "))
+number2 = int(input("Second number: "))
+
+maximum = max(number1, number2)
+print("The maximum number is", maximum)
